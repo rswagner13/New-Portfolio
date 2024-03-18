42df3ad0cb9e0bb1acb0a72aae8aea99b9d5ddfb
Mour Wagner <mourwagner13@gmail.com>
2024-03-18 11:52:39 -0400

    Making first commit of new portfolio to GitHub

---
+++ src/Components/App/index.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './styles.css'
+
+export default function App() {
+
+
+  return (
+    <>
+    <header className="navbar">
+      <img src="" itemID="head-photo"/>
+      <h1 id="nav-name">Nav Title</h1>
+
+    </header>
+      <h1>Hello World</h1>
+    </>
+  )
+}
